b77032347bb0d89afd2757ef169676d3aec05652
Dylan Kentish <dylankentish@live.co.uk>
2022-08-18 14:22:02 +0100

    Added Categories Page
    Experimented using the CollectionView

---
+++ MauiApp1/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiApp1.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+        }
+
+        public Category(string name, string uri)
+        {
+            Name = name;
+            Uri = uri;
+        }
+
+        public string Name { get; set; }
+        public string Uri { get; set; }
+    }
+}
